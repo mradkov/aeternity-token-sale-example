aa67e4c7d392889d557156b764c6fb5e13056fe6
Philipp Piwo <github@piwo.space>
2019-11-20 13:59:41 +0200

    added loading and transfer

---
+++ token-sale-aepp/src/components/BiggerLoader.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="mt-8 relative flex justify-center">
+    <img src="../assets/loading_logo.svg">
+  </div>
+</template>
+
+<script>
+
+  import { AeLoader } from '@aeternity/aepp-components'
+
+  export default {
+    name: "BiggerLoader",
+    components: {AeLoader}
+  }
+</script>
+
+<style scoped>
+
+</style>
--- token-sale-aepp/src/views/Home.vue
+++ token-sale-aepp/src/views/Home.vue
@@ -1,61 +1,90 @@
 <template>
-  <div class="p-2">
-    <ae-identity class="bg-primary" v-bind="identity" collapsed/>
-    <h1 class="h1">Sample Token Sale (STT)</h1>
-    <div>This is a sample token sale on aeternity testnet, each token sells for
-      <span v-if="price" class="font-bold">{{price}}</span>
-      <ae-loader v-else/>
-      <span class="font-bold">AEtto</span>.
-      These tokens don't carry any value and should only function as example showcase on testnet.
+  <div>
+    <div class="overlay-loader" v-show="showLoading">
+      <BiggerLoader></BiggerLoader>
     </div>
+    <div class="p-2">
+      <ae-identity class="bg-primary" v-bind="identity" collapsed/>
+      <h1 class="h1">Sample Test Token Sale</h1>
+      <div>This is a sample token sale on aeternity testnet, each token sells for
+        <span v-if="price" class="font-bold">{{price}}</span>
+        <ae-loader v-else/>
+        <span class="font-bold">AEtto</span>.
+        These tokens don't carry any value and should only function as example showcase on testnet.
+      </div>
 
-    <h1 class="h1">STT Balance:
-      <span v-if="sttbalance" class="font-bold">{{sttbalance}}</span>
-      <ae-loader v-else/>
-    </h1>
+      <h1 class="h1">STT Balance:
+        <span v-if="sttbalance !== null" class="font-bold">{{sttbalance}}</span>
+        <ae-loader v-else/>
+      </h1>
 
-    <ae-input label="Amount of Tokens to Buy" v-model="amount"/>
-    <ae-button face="round" fill="primary" extend @click="contribute()">Buy {{amount}} STT for {{amount/price}} AEtto</ae-button>
+      <ae-input label="Amount of Tokens to Buy" v-model="buyAmount"/>
+      <ae-button face="round" fill="primary" extend @click="contribute()">Buy {{buyAmount}} STT for {{buyAmount/price}}
+        AEtto
+      </ae-button>
+
+      <div class="flex mt-3">
+        <div class="w-1/4">
+          <ae-input label="Amount" v-model="transferAmount"/>
+        </div>
+        <div class="w-3/4">
+          <ae-input label="Address" v-model="transferAddress"/>
+        </div>
+      </div>
+      <ae-button face="round" fill="primary" extend @click="send()">Send {{transferAmount}} STT</ae-button>
+    </div>
   </div>
 </template>
 
 <script>
     import aeternity from "../utils/aeternity";
-    import {AeIdentity, AeLoader, AeInput, AeButton} from "@aeternity/aepp-components"
+    import {AeIdentity, AeLoader, AeInput, AeButton, AeButtonGroup} from "@aeternity/aepp-components";
+    import BiggerLoader from "../components/BiggerLoader";
 
     export default {
         name: 'Home',
-        components: {AeIdentity, AeLoader, AeInput, AeButton},
+        components: {AeIdentity, AeLoader, AeInput, AeButton, AeButtonGroup, BiggerLoader},
         data() {
             return {
-                amount: 1,
+                error: null,
+                showLoading: false,
+                buyAmount: 1337,
                 price: null,
                 sttbalance: null,
                 identity: null,
-                aeternity: null
+                transferAmount: 0,
+                transferAddress: null
             };
         },
         methods: {
             async refresh() {
-                await aeternity.initClient();
-
-                if (aeternity.isTestnet() && aeternity.balance <= 5) {
-                    await axios.post(`https://testnet.faucet.aepps.com/account/${aeternity.address}`, {}, {headers: {'content-type': 'application/x-www-form-urlencoded'}}).catch(console.error);
-                }
                 this.identity = {
                     address: aeternity.address,
                     balance: parseInt(aeternity.balance)
                 };
-                this.aeternity = aeternity;
-                this.price = (await aeternity.saleContract.methods.price()).decodedResult;
                 this.sttbalance = (await aeternity.tokenContract.methods.balance(aeternity.address)).decodedResult || 0;
+                this.showLoading = false;
             },
             async contribute() {
-                await this.aeternity.saleContract.methods.contribute({amount: this.amount * this.price});
+                this.showLoading = true;
+                await aeternity.saleContract.methods.contribute({amount: this.buyAmount * this.price});
+                this.refresh();
+            },
+            async send() {
+                this.showLoading = true;
+                await aeternity.tokenContract.methods.transfer(this.transferAddress, this.transferAmount);
                 this.refresh();
             }
         },
         async mounted() {
+            this.showLoading = true;
+            await aeternity.initClient();
+
+            if (aeternity.isTestnet() && aeternity.balance <= 5) {
+                await axios.post(`https://testnet.faucet.aepps.com/account/${aeternity.address}`, {}, {headers: {'content-type': 'application/x-www-form-urlencoded'}}).catch(console.error);
+            }
+
+            this.price = (await aeternity.saleContract.methods.price()).decodedResult;
             this.refresh();
         },
     };
